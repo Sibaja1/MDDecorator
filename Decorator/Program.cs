633af8573a7eb782917c0174de64127d4b6210ce
Sibaja1 <104949837+Sibaja1@users.noreply.github.com>
2023-06-19 10:24:25 -0600

    Add files via upload

---
+++ Decorator/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    // Componente: Interfaz para la bebida
+    public interface IBebida
+    {
+        string ObtenerDescripcion();
+        double ObtenerPrecio();
+    }
+
+    // ComponenteConcreto: Implementación de una bebida
+    public class Cafe : IBebida
+    {
+        public string ObtenerDescripcion()
+        {
+            return "Café";
+        }
+
+        public double ObtenerPrecio()
+        {
+            return 2.5;
+        }
+    }
+
+    // Decorador: Clase base abstracta para decoradores
+    public abstract class DecoradorBebida : IBebida
+    {
+        protected IBebida _bebida;
+
+        public DecoradorBebida(IBebida bebida)
+        {
+            _bebida = bebida;
+        }
+
+        public virtual string ObtenerDescripcion()
+        {
+            return _bebida.ObtenerDescripcion();
+        }
+
+        public virtual double ObtenerPrecio()
+        {
+            return _bebida.ObtenerPrecio();
+        }
+    }
+
+    // DecoradorConcreto: Decorador para agregar leche
+    public class LecheDecorator : DecoradorBebida
+    {
+        public LecheDecorator(IBebida bebida) : base(bebida)
+        {
+        }
+
+        public override string ObtenerDescripcion()
+        {
+            return base.ObtenerDescripcion() + ", Leche";
+        }
+
+        public override double ObtenerPrecio()
+        {
+            return base.ObtenerPrecio() + 0.5;
+        }
+    }
+
+    // DecoradorConcreto: Decorador para agregar azúcar
+    public class AzucarDecorator : DecoradorBebida
+    {
+        public AzucarDecorator(IBebida bebida) : base(bebida)
+        {
+        }
+
+        public override string ObtenerDescripcion()
+        {
+            return base.ObtenerDescripcion() + ", Azúcar";
+        }
+
+        public override double ObtenerPrecio()
+        {
+            return base.ObtenerPrecio() + 0.25;
+        }
+    }
+
+    // Uso del patrón Decorator
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Creamos una bebida base
+            IBebida cafe = new Cafe();
+
+            // Agregamos decoradores para personalizar la bebida
+            cafe = new LecheDecorator(cafe);
+            cafe = new AzucarDecorator(cafe);
+
+            // Mostramos la descripción y el precio de la bebida
+            Console.WriteLine(cafe.ObtenerDescripcion());
+            Console.WriteLine("Precio: $" + cafe.ObtenerPrecio());
+            Console.ReadLine();
+        }
+    }
+
+}
